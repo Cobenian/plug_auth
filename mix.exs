4c64890b257bc7ce997543d9c6c110cf0113d941
Bryan Weber <brweber2@yahoo.com>
2015-02-17 11:50:28 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule PlugAuth.Mixfile do
 
   defp deps do
     [
-      {:cowboy, "~> 1.1"}, 
+      {:cowboy, "~> 1.0"}, 
       {:plug, "~> 0.10.0"},
       {:earmark, "~> 0.1", only: :docs},
       {:ex_doc, "~> 0.7.1", only: :docs},
